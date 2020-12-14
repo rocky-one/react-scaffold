f97bd19f0a7fdd228f79ee57a72a141d4dace49f
rocky-one <zuihongyanxiangsi@163.com>
2020-12-14 21:23:51 +0800

    button

---
+++ src/components/button1/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './css';
+
+function Button1() {
+  return <div className="button1">button1</div>;
+}
+
+export default Button1;
---
+++ src/components/button2/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './css';
+
+function Button2() {
+  return <div className="button2">button2</div>;
+}
+
+export default Button2;
